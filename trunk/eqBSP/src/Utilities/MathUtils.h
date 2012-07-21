43c74f494dd42439d55a6c8b173d1ba525d9615a
matteo.hausner@gmail.com <matteo.hausner@gmail.com@eba8a8f8-3d87-631c-c242-5909fbef99e8>
2012-07-21 19:35:07 +0000

    -Rendering patches -etc.

--- trunk/eqBSP/src/Utilities/MathUtils.h
+++ trunk/eqBSP/src/Utilities/MathUtils.h
@@ -16,3 +16,37 @@ float dotProduct(const float a[3], const float b[3]) {
 Matrix4 worldToCameraSpace(Matrix4 modelMatrix, Matrix4 viewMatrix) {
 	return viewMatrix * modelMatrix;
 }
+
+// Vertex addition
+TVertex vertexAddition(TVertex a, TVertex b) {
+	TVertex result;
+
+	for(int i = 0; i < 2; i++)
+		result.mPosition[i] = a.mPosition[i] + b.mPosition[i];
+
+	// TODO Do missing additions:
+//	result.position=position+rhs.position;
+//	result.decalS=decalS+rhs.decalS;
+//	result.decalT=decalT+rhs.decalT;
+//	result.lightmapS=lightmapS+rhs.lightmapS;
+//	result.lightmapT=lightmapT+rhs.lightmapT;
+
+	return result;
+}
+
+// Vertex multiplication
+TVertex vertexMultiplication(TVertex v, float f) {
+	TVertex result;
+
+	for(int i = 0; i < 2; i++)
+		result.mPosition[i] = v.mPosition[i] * f;
+
+	// TODO Do missing multiplications:
+//	result.position=position*rhs;
+//	result.decalS=decalS*rhs;
+//	result.decalT=decalT*rhs;
+//	result.lightmapS=lightmapS*rhs;
+//	result.lightmapT=lightmapT*rhs;
+
+	return result;
+}
